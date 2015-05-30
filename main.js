3f739a391e9afedc3fb93c92b9d1cc78378651a3
Jesse Jurman <j.r.jurman@gmail.com>
2015-05-30 12:39:54 -0400

    bird is the word

--- main.js
+++ main.js
@@ -32,10 +32,6 @@ var checkKey = function(e){
 var checkWord = function(){
   var word = document.getElementById('focusedInput').value.toUpperCase();
 
-  if (word == "BIRD") {
-    window.open('https://www.youtube.com/watch?v=UAJEbuTBodI','_blank');
-  }
-
   if (dictionaryString.indexOf(word) > -1) {
     document.getElementById('check').classList.add("btn-success");
     document.getElementById('check').innerHTML = word + " is a word";
@@ -43,5 +39,10 @@ var checkWord = function(){
   else {
     document.getElementById('check').classList.add("btn-primary");
     document.getElementById('check').innerHTML = word + " is not a word";
+  }
+
+  if (word == "BIRD") {
+    window.open('https://www.youtube.com/watch?v=UAJEbuTBodI','_blank');
+    document.getElementById('check').innerHTML = word + " is the word";
   }
 }
